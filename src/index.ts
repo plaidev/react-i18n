65919c47d9ab747a13ca2163431cbcd8a1cb104d
RyosukeCla <RyosukeCla@users.noreply.github.com>
2022-10-25 18:15:21 +0900

    expose cookie

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export {
 export type { I18ProviderProps } from "./provider";
 export { useTranslation } from "./translation";
 export type { Locale, Currency } from "./locales";
-export { browser, ignorePlaceCode, forcedLocale } from "./detectors";
+export { browser, ignorePlaceCode, forcedLocale, cookie } from "./detectors";
 export type { Detector } from "./detectors";
 export {
   useCurrencyFormat,
