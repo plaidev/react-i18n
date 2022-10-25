f4adf4ecef8d3f50c0826a83b0ba5f5f9f36a5ac
RyosukeCla <RyosukeCla@users.noreply.github.com>
2022-10-25 19:52:34 +0900

    fix

--- src/translation.ts
+++ src/translation.ts
@@ -3,6 +3,10 @@ import type { Locale } from "./locales";
 import { useLocaleSetting } from "./provider";
 
 const VITE_PLUGIN_REACT_PREAMBLE_INSTALLED = (window as any).__vite_plugin_react_preamble_installed__ as boolean | undefined;
+if (VITE_PLUGIN_REACT_PREAMBLE_INSTALLED) {
+  console.warn("@plaidev/react-i18n: @vitejs/plugin-react is detected, so useTranslation won't cache the resources. However, it will be cached on production build.");
+}
+
 export const useTranslation = <Resource, Locales extends Locale>(resources: {
   [key in Locales]: Resource;
 }): Resource => {
@@ -13,7 +17,6 @@ export const useTranslation = <Resource, Locales extends Locale>(resources: {
    * VITE_PLUGIN_REACT_PREAMBLE_INSTALLED is always the same in the context, so we can use useEffect in a if condition.
    */
   if (VITE_PLUGIN_REACT_PREAMBLE_INSTALLED) {
-    console.warn("@plaidev/react-i18n: vite react plugin is detected, so useTranslation won't cache the resources. However, it will be cached on production build.");
     useEffect(() => {
       setResources(resources);
     }, [resources]);
